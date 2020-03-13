bab2ed2f06745a295d98a9f3d79c1a2629ad5429
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-03-13 09:45:38 -0700

    added validateAction middleware, post request for actions

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Projects = require('../data/helpers/projectModel');
+const Actions = require('../data/helpers/actionModel');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -29,6 +30,18 @@ router.post('/', validateProject, (req, res) => {
       res
         .status(500)
         .json({ message: 'There was an error adding the project.' });
+    });
+});
+
+router.post('/:id/posts', validateProjectId, validateAction, (req, res) => {
+  const action = req.body;
+  Actions.insert(action)
+    .then(action => res.status(200).json(action))
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: 'There was an error adding the action.' });
     });
 });
 
@@ -65,6 +78,33 @@ function validateProjectId(req, res, next) {
         .status(500)
         .json({ message: 'There was an error retrieving the project.' });
     });
+}
+
+function validateAction(req, res, next) {
+  const action = req.body;
+  const { id } = req.params;
+  console.log(action);
+  !action ? res.status(400).json({ message: 'missing action data' }) : null;
+  !action.project_id
+    ? res.status(400).json({ message: 'missing required field: project_id' })
+    : null;
+  action.project_id !== id
+    ? res
+        .status(400)
+        .json({ message: 'project_id does not match id in post request' })
+    : null;
+  !action.description
+    ? res.status(400).json({ message: 'missing required field: description' })
+    : null;
+  action.description.length > 128
+    ? res
+        .status(400)
+        .json({ message: 'description may not be longer than 128 characters' })
+    : null;
+  !action.notes
+    ? res.status(400).json({ message: 'missing required field: notes' })
+    : null;
+  next();
 }
 
 module.exports = router;
