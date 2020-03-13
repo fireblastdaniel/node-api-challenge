c474341457a8aa458dcb3e15c357d3129e87333b
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-03-13 10:38:58 -0700

    completed remaining CRUD requests for actions

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -3,4 +3,57 @@ const express = require('express');
 const Actions = require('../data/helpers/actionModel');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  Actions.get()
+    .then(actionsList => {
+      res.status(200).json(actionsList);
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: 'There was an error retrieving the user data.' });
+    });
+});
+
+router.get('/:id', validateActionId, (req, res) => {
+  res.status(200).json(req.action);
+});
+
+router.put('/:id', validateActionId, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  Actions.update(id, changes)
+    .then(updatedAction =>
+      updatedAction
+        ? res.status(200).json(updatedAction)
+        : res.status(400).json({ message: 'Unable to update the action' })
+    )
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: 'There was an error updating the action.' });
+    });
+});
+
+function validateActionId(req, res, next) {
+  const { id } = req.params;
+  Actions.get(id)
+    .then(action => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        res.status(400).json({ message: 'invalid action id' });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: 'There was an error retrieving the user' });
+    });
+}
+
 module.exports = router;
